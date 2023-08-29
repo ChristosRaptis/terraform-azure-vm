ce9a08e5159458784b9d1a6601895fc52521fd73
Christos Raptis <chrisgraptis@gmail.com>
2023-08-29 16:39:28 +0200

    Added files

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "resource_group_name" {
+  default = "rg-bouman6_7"
+}
+
+variable "virtual_network_name" {
+  default = "terraform_network"
+}
+variable "prefix" {
+  default = "tf_azure_test"
+}
+variable "location" {
+  default = "francecentral"
+}
